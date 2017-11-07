4151f89f411f7b9a9d46b8b6547345764eb45a49
malelang <landiezm@unicauca.edu.co>
2017-11-07 00:41:59 -0500

    Primer commit

---
+++ PrettyPets/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PrettyPets
+//
+//  Created by Alejandra Landinez on 11/6/17.
+//  Copyright © 2017 LordBoxen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var username: UITextField!
+    @IBOutlet var pass: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func goToRegister(_ sender: UIButton) {
+    }
+    
+}
+
